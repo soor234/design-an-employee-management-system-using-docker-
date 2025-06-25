ff5389b3191740a38832862b48a393be82b648fa
surya narayanan <74855435+soor234@users.noreply.github.com>
2025-06-25 21:13:55 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    role VARCHAR(100),
+    department VARCHAR(100)
+);
+create TABLE surya (
+   id int STRING  AUTO INCREMENT PRIMAARY KEY;
+  NAME  Varchar(20)
+  department  varchar?920)
+);
